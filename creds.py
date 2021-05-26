86668af58badd08dba211e0d16a1b3c52c75130a
Ayan Upadhaya <ayanu881@gmail.com>
2021-05-26 23:24:10 +0600

    added api paste format and api folder key

--- creds.py
+++ creds.py
@@ -1,4 +1,7 @@
 #your api dev key and api user key should be in the following variables
+#your api paste format and api foler key should be in the following variables
 #you have to import this file in the main file - pastebinautomation.py
 api_dev_key  = ""
 api_user_key = ""
+api_paste_format = ""
+api_folder_key = ""
